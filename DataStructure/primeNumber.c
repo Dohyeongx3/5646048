6cf758c335d4b7c192eb95587c6c6d2722e66853
mnb33300@stu.kmu.ac.kr <mnb33300@stu.kmu.ac.kr>
2024-03-12 10:03:29 +0900

    소수 판별 프로그램 추가

---
+++ DataStructure/primeNumber.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+//소수 판별 함수
+bool isPrime(int num) {
+	if (num <= 1) {
+		return false;    // 1 이하의 수는 소수가 아닙니다.
+	}
+	for (int i = 2; i * i <= num; i++) {
+		if (num % i == 0) {
+			return false; // 소수가 아닙니다.
+		}
+	}
+	return true; //소수 입니다.
+}
+
+int main(void) {
+	printf("0부터 100까지의 소수: \n");
+	for (int i = 0; i <= 100; i++) {
+		if (isPrime(i)) {
+			printf("%d ", i);  //0부터 100까지 소수 출력
+		}
+	}
+	printf("\n");
+	return 0;
+}
